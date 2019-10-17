76eed701cb53c5245d584edf4a4307fbfeaca18c
jondauz <jondauz@gmail.com>
2019-10-17 16:44:01 -0700

    more styles and UI

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import './ProjectCard.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
 
 class ProjectCard extends React.Component {
+  
+  getProfileImage(imageUrl) {
+    return imageUrl.replace('{function}', 'crop').replace('{size}', '80x80');
+  }
+
   render() {
     let { data } = this.props;
     return(
       <article className="project-card">
+        <header>
+          <img src={this.getProfileImage(data.profile.avatar.dynamic)} alt={`${data.profile.firstname} ${data.profile.lastname}`} />
+          {data.profile.firstname} {data.profile.lastname}
+        </header>
         <div className="project-card__image" style={{
           backgroundImage:`url(${data.media[0].filename.url})`
         }}>
         </div>
         <div className="project-card__content">
-          <h3>{data.title}</h3>
-          <p>{data.description}</p>
+          <div className="project-card__title">
+            {data.title}
+          </div>
+          <div className="project-card__desc">
+            {data.description}
+          </div>
+          </div>
+        <div className="project-card__category">
+          {data.category}
         </div>
+        <footer>
+          <FontAwesomeIcon icon={faHeart} /> 
+          <span>{data.likes}</span>
+          <FontAwesomeIcon icon={faShareAlt} />
+        </footer>
       </article>
     )
   }
